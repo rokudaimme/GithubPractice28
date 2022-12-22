02d98aec4af70d599db7f776b3b44a79454fd380
muhammet aydin <112701546+maydin61@users.noreply.github.com>
2022-12-22 19:43:47 +0200

    Update Task3.java

--- src/Task3.java
+++ src/Task3.java
@@ -3,5 +3,8 @@ public class Task3 {
     public static void main(String[] args) {
 
         System.out.println("Third line of our code");
+        
+        System.out.println("When can we start to 4.th line code ");
+        
     }
 }
